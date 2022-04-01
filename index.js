11ac528c0e1a3acd462feed9b94a874dbbebd2c8
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-02 01:06:29 +0530

    made console working

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 
+function compareValues(sum,luckyNumber) {
+    if(sum % luckyNumber === 0) {
+        console.log("Your birthday is lucky !😍")
+    }
+    else {
+        console.log("Your birthday is not lucky !🤮")
+    }
+}
+
 function checkBirthDateisLucky() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
+    compareValues(sum,luckyNumber.value);
 }
 
 function calculateSum(dob) {
