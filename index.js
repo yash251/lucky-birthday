308318df7845e783730a3d64788a1b317a3519e7
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-02 02:06:53 +0530

    final

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const checkNumber = document.querySelector("#check-number");
-const outputBox = document.querySelector("output-box");
+const checkButton = document.querySelector("#check-number");
+const displayMessage = document.querySelector("#output-box");
 
-function compareValues(sum,luckyNumber) {
-    if(sum % luckyNumber === 0) {
-        outputBox.value = "Your birthday is lucky !😍";
-    }
-    else {
-        outputBox.value = "Your birthday is not lucky !🤮";
-    }
-}
-
-function checkBirthDateisLucky() {
-    const dob = dateOfBirth.value;
-    const sum = calculateSum(dob);
-    if(sum && dob)
-    compareValues(sum,luckyNumber.value);
-}
-
-function calculateSum(dob) {
-    dob = dob.replaceAll("-", "");
-    let sum = 0;
-    for(let index=0;index<dob.length;index++) {
-        sum = sum + Number(dob.charAt(index));
+const calculateSum = (date) => {
+    let sum=0;
+    date = date.replaceAll("-","");
+    for(let digit of date) {
+        sum = sum + Number(digit);
     }
     return sum;
 }
 
-checkNumber.addEventListener('click', checkBirthDateisLucky)
+const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
+    console.log(sumOfDate, numberToCheck)
+    if(sumOfDate % numberToCheck === 0) {
+        return showMessage(`${numberToCheck} is a lucky number! 😍 `)
+    }
+    else {
+        showMessage(`${numberToCheck} is not that lucky 🤮. Make your own luck`);
+    }
+}
+
+const showMessage = (message) => {
+    displayMessage.innerText = message;
+}
+
+checkButton.addEventListener("click", () => {
+    const date = dateOfBirth.value;
+    const numberToCheck = luckyNumber.value;
+    if(date && numberToCheck) {
+        const sumOfDate = calculateSum(date);
+        checkIsNumberLucky(sumOfDate, numberToCheck);
+    }
+    else {
+        showMessage("Please enter both the fields");
+    }
+})
